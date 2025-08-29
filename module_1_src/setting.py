71107e345f529ddf623d6b7ec1af03bddad5f3be
Tata-hub1011 <tanechka.101190@gmail.com>
2025-08-29 17:21:01 +0800

    module 1 done

---
+++ module_1_src/setting.py
@@ -0,0 +1,7 @@
+C:\Users\tanys\git_tutorial>mkdir module_1_src
+
+C:\Users\tanys\git_tutorial>cd module_1_src
+
+C:\Users\tanys\git_tutorial\module_1_src>type nul > setting.py
+
+C:\Users\tanys\git_tutorial\module_1_src>type nul urls.py
---
+++ module_1_src/urls.py
@@ -0,0 +1,5 @@
+
+C:\Users\tanys\git_tutorial>git checkout module_1
+Switched to branch 'module_1'
+
+C:\Users\tanys\git_tutorial>mkdir module_1_src
